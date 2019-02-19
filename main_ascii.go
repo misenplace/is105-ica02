d94b508f9b2f707279dcf42725cee42d28420c39
Runar Grønås <35918203+anzuh@users.noreply.github.com>
2019-02-19 12:00:00 +0100

    Simplify main_ascii

--- main_ascii.go
+++ main_ascii.go
@@ -2,36 +2,8 @@ package main
 
 import (
 	"github.com/anzu/is105-ica02/ascii"
-	"os"
-	"fmt"
 )
 
 func main() {
-
-	// if arguments passed is less than 2, print usage
-	// os.Args counts the actual program name (main_ascii.go) as an argument.
-	// therefore, we have to check if it's more than two.
-	// this check is required to not get an IndexOutOfBoundsException
-	if len(os.Args) < 2 {
-		printUsage()
-	} else {
-//		// if second argument is -ex, print extended ascii table
-//		if os.Args[1] == "-ex" {
-//			ascii.IterateOverExtendedASCIIStringLiteral(ascii.GetExtendedASCIIStringLiteral())
-//		// if second argument is -st, print standard ascii table
-//		} else 
-		if os.Args[1] == "-st" {
-			ascii.IterateOverASCIIStringLiteral(ascii.GetStandardASCIIStringLiteral())
-		// if second argument is nothing at all, print usage.
-		} else {
-			printUsage()
-		}
-	}
-}
-
-// usage function
-func printUsage() {
-	fmt.Println("usage: main_ascii [-st][-ex]")
-	fmt.Println("-st	prints standard 0 to 127 ascii table")
-	fmt.Println("-ex	prints exnteded ascii table")
+	ascii.IterateOverASCIIStringLiteral(ascii.GetStandardASCIIStringLiteral())
 }
