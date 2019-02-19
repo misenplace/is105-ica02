d9388f0445ca65821e9b4012dd67f59be06bd12e
Runar Grønås <35918203+anzuh@users.noreply.github.com>
2019-02-19 11:59:33 +0100

    Simplify test

--- iso/iso_test.go
+++ iso/iso_test.go
@@ -2,18 +2,13 @@ package iso
 
 import (
 	"testing"
-	"unicode"
 )
 
-// checks if string is ONLY extended ascii
-// this will usually fail, as it requires the string be ONLY extended ascii
-// and no normal chars
 func TestGreetingExtendedASCIIONly(t *testing.T) {
 	val := GreetingExtendedASCII()
-	for i := 0; i < len(val); i++ {
-		// if val[i] is not between hex 80 and hex FF throw error
-		if !(val[i] >= unicode.MaxASCII && val[i] <= unicode.MaxLatin1 ) {
-			t.Errorf("greetingExtendedASCII() returns non-extended ASCII value: %q", val[i])
+	for _, i := range val {
+		if !(i > 0x80 && i < 0xff) {
+			t.Errorf("greetingExtendedASCII() returns non-extended ASCII value: %q - %v", i, i)
 		}
 	}
 }
